153e46b15b09cb8f5db5958988a8247da276d079
swufebear <114322509+swufebear@users.noreply.github.com>
2024-03-15 17:12:35 +0000

    Add schema

--- src/components/Cell/CellBoard.tsx
+++ src/components/Cell/CellBoard.tsx
@@ -24,4 +24,77 @@ const CellRow = styled.div`
 const CellsContainer = styled.div`
     display: flex;
     flex-direction: row;
-   
+    gap: 8px;
+    flex-grow: 1;
+`;
+
+const CellRowControls = styled.div`
+    display: flex;
+`;
+
+const AddColumnButton = styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 32px;
+    width: 32px;
+    border-radius: 8px;
+    background-color: #0088ff99;
+    color: #ffffff;
+    font-size: 24px;
+    font-weight: bold;
+    border: none;
+    cursor: pointer;
+    &:hover {
+        background-color: #0088ff;
+    }
+    &:disabled {
+        background-color: #dddddd;
+        cursor: auto;   
+    }
+`;
+
+const AddRowButton = styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 32px;
+    width: 160px;
+    border-radius: 8px;
+    background-color: #0088ff99;
+    color: #ffffff;
+    font-size: 16px;
+    border: none;
+    cursor: pointer;
+    &:hover {
+        background-color: #0088ff;
+    }
+    &:disabled {
+        background-color: #dddddd;
+        cursor: auto;   
+    }
+`;
+
+interface CellBoardProps {
+    initialBoard: string[][];
+    maxRows: number;
+    maxColumns: number;
+    setEntryCell: (cellId: string | undefined) => void;
+    style?: React.CSSProperties;
+}
+
+const CellBoard: React.FC<CellBoardProps> = ({
+    initialBoard, 
+    maxRows, 
+    maxColumns, 
+    setEntryCell,
+    style
+}) => {
+    const [board, setBoard] = React.useState<string[][]>([]); // 2d array of cell ids
+    const [activeCells, setActiveCells] = React.useState<(string | undefined)[]>([]); // array of cell ids or undefined for each row
+    const { cells, addCell, linkCells, unlinkCell, toggleCell } = React.useContext(ObjectsContext);
+
+    // initialize board
+    React.useEffect(() => {
+        var newBoard = initialBoard.map(row => row.map(text => {
+            
