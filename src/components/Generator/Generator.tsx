fe0ac716954d7b26fe8b11e12262b25ce100e3af
swufebear <114322509+swufebear@users.noreply.github.com>
2024-04-13 13:38:39 +0000

    0.13.1

--- src/components/Generator/Generator.tsx
+++ src/components/Generator/Generator.tsx
@@ -81,4 +81,45 @@ const ControllerContainer = styled.div<{ size: string, row: number, column: numb
     left: ${(props) => props.size === "small" ? 4 + props.column*(36 + 4) : (props.size == "medium" ? 6 + props.column*(54 + 6) : 8 + props.column*(72 + 8))}px;
     background-color: rgba(255, 255, 255, 0.95);
     border-radius: ${(props) => props.size === "small" ? "2px" : (props.size == "medium" ? "4px" : "6px")};
-    height: ${(props) => props.size === "smal
+    height: ${(props) => props.size === "small" ? "34px" : (props.size == "medium" ? "51px" : "68px")};
+    width: ${(props) => props.size === "small" ? "120px" : (props.size == "medium" ? "180px" : "220px")};
+    border: solid ${(props) => props.color} ${(props) => props.size === "small" ? "1px" : "2px"};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    box-sizing: border-box;
+    box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+    display: flex;
+    flex-direction: column;
+    font-size: ${(props) => props.size === "small" ? "10px" : (props.size === "medium" ? "16px" : "20px")};
+    text-align: left;
+    cursor: pointer;
+    z-index: 3;
+`;
+
+const ControllerInner = styled.div<{size: string, filled?: number}>`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    width: calc(100% - ${(props) => props.size === "small" ? "8px" : (props.size === "medium" ? "16px" : "24px")});
+    padding: ${(props) => props.size === "small" ? "0 4px" : (props.size == "medium" ? "0 8px" : "0 12px")};
+
+    & > input[type=text] {
+        font-size: ${(props) => props.size === "small" ? "10px" : (props.size == "medium" ? "14px" : "18px")};
+        width: 20%;
+        text-align: center;
+        border: solid 1px #cccccc;
+        border-radius: 4px;
+    }
+
+    & > input[type=range] {
+        width: 100%;
+        -webkit-appearance: none;
+        padding-top: ${(props) => props.size === "small" ? "2px" : (props.size === "medium" ? "4px" : "6px")};
+    }
+    & > input[type=range]::-webkit-slider-thumb {
+        -webkit-appearance: none;
+        appearance: none;
+        width: ${(props) => props.size === "small" ? "8px" : (props.size === "medium" ? "12px" : "16px")};
+        height: ${(props) => props.size === "small
