fd2e10f6695fcf59640a2c4b39a22887ba90cd5f
swufebear <114322509+swufebear@users.noreply.github.com>
2024-05-08 10:05:28 +0000

    fix #18

--- src/context/ObjectsContextProvider.test.tsx
+++ src/context/ObjectsContextProvider.test.tsx
@@ -11,4 +11,56 @@ describe("ObjectsContextProvider", () => {
     var mockContextProvider: {
         generators: any[];
         lenses: any[];
-        generateHandler:
+        generateHandler: (input: string | string[], parameters: any) => Promise<string | string[]>;
+    } = {
+        generators: [],
+        lenses: [],
+        generateHandler: async (input: string | string[], parameters: any) => {
+            return "output";
+        }
+    };
+
+    const MockChild = () => {
+        const { generators, lenses } = React.useContext<IObjectsContext>(ObjectsContext);
+
+        return (
+            <div data-testid="ChildComponent">
+                {generators.map((generator: any) => (
+                    <Generator 
+                        key={generator.id}
+                        {...generator}
+                    />
+                ))}
+                {lenses.map((lens: any) => (
+                    <Lens 
+                        key={lens.id}
+                        {...lens}
+                        onGenerationClick={(generation: string) => {}}
+                    />
+                ))}
+            </div>
+        );
+    }
+
+    test("renders ObjectsContextProvider component", () => {
+        render(
+            <ObjectsContextProvider {...mockContextProvider}>
+                <MockChild/>
+            </ObjectsContextProvider>
+        );
+        expect(screen.getByTestId("ChildComponent")).toBeInTheDocument();
+    });
+
+    mockContextProvider = {
+        ...mockContextProvider,
+        generators: [
+            {
+                id: "Generator-id",
+                parameters: [
+                    {
+                        id: "model",
+                        name: "Model",
+                        value: "gpt-3.5-turbo",
+                        type: "nominal",
+                        allowedValues: ["gpt-3.5-turbo", "gpt-4", "text-davinci-003"],
+                        valueNicknames: {"gpt-
